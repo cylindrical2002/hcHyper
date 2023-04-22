336a99c1d52f2719b6c12f1c203875924a4a3301
Yue Chen <chen-yue20@mails.tsinghua.edu.cn>
2023-04-22 14:32:10 +0800

    Remove hcMinikernel

--- guest/hcMiniKernel/os/src/syscall/process.rs
+++
@@ -1,8 +0,0 @@
-//! App management syscalls
-use crate::batch::run_next_app;
-
-/// task exits and submit an exit code
-pub fn sys_exit(exit_code: i32) -> ! {
-    trace!("[kernel] Application exited with code {}", exit_code);
-    run_next_app()
-}
