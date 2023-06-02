00e858d228ccc8f4b96410f93062bfbf37e3982c
Yue Chen <chen-yue20@mails.tsinghua.edu.cn>
2023-06-02 22:59:57 +0800

    remove user and dtb

--- user/c/include/pthread.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PTHREAD_H__
-#define __PTHREAD_H__
-
-typedef unsigned long pthread_t;
-
-int pthread_create(pthread_t *res, const void *attrp, void *(*entry)(void *), void *arg);
-
-#endif // __PTHREAD_H__
--- user/c/src/hello_c.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-    puts("Hello, world!");
-    return 0;
-}
